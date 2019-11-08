5dfd13f73bc3f0dd1ac035fb9d77ea17598e221c
Keila Fernandes <31359251+Keemluvr@users.noreply.github.com>
2019-11-08 11:45:51 -0300

    Add files via upload

---
+++ src/main/java/ifsc/projetos/pesquisa/sd/cardapioifsc/CardapioIfscApplication.java
@@ -0,0 +1,13 @@
+package ifsc.projetos.pesquisa.sd.cardapioifsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioIfscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioIfscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifsc/projetos/pesquisa/sd/cardapioifsc/CardapioIfscApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsc.projetos.pesquisa.sd.cardapioifsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioIfscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
